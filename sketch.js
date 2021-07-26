900c173f298f01cc773086cfbc1e97f4fb5c0a6b
Romain <monaco.rom1@gmail.com>
2021-07-26 12:05:35 +0200

    Bloquage des mur du jeu

--- sketch.js
+++ sketch.js
@@ -1,20 +1,68 @@
-// let img;
-// function preload() {
-//     img = loadImage('Images/fond-slide-ludispace.jpg');
-// }
-posX = 550;
-posY = 50;
+let posX, posY;
 
 function setup() {
+    posX = 100; 
+    posY = 100;
     createCanvas(640, 480);
-    background(153);
-    line(0, 0, width, height);
-
-    // Image(img, 0, 0);
+    
+    fill(255, 0, 0);
+    background(255, 204, 0);
 }
+
 function draw() {
-    colorMode(HSB);
     background(255, 204, 100);
-    ellipse(50,50,80,80);
+    updatePositionCercle();
+    testOutOfScreen();
+    ellipse(50, 50, 80, 80);
     circle(posX, posY, 50);
 }
+
+function updatePositionCercle() {
+   
+    if (keyIsDown(LEFT_ARROW)) {
+        posX -= 5;
+        console.log("flèche de droite appuyée")
+    } else {
+        console.log("pas appuyée");
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        posX += 5;
+        console.log("flèche de gauche appuyée")
+    } else {
+        console.log("pas appuyée");
+    }
+    if (keyIsDown(UP_ARROW)) {
+        posY -= 5;
+        console.log("flèche du haut appuyée")
+    } else {
+        console.log("pas appuyée");
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+        posY += 5;
+        console.log("flèche du bas appuyée")
+    } else {
+        console.log("pas appuyée");
+    }
+}
+
+function testOutOfScreen() {
+    
+    if (posX < 0) {
+       posX = 0; 
+    }
+    if (posX > 640) {
+        posX = 640;
+    }
+    if (posY < 0 ) {
+        posY = 0 ;
+    }
+    if (posY > 480) {
+        posY = 480;
+    }
+}
+
+
+    
+
+
